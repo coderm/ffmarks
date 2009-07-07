a45fae764489b3ae0cf356df28006ce2036cc00c
Berker Peksag <berker.peksag@gmail.com>
2009-07-08 00:36:42 +0300

    JavaScipt files updated.

--- application/views/view_header.php
+++ application/views/view_header.php
@@ -14,26 +14,6 @@
 	google.load("jquery", "1.3.2");
 	google.load("language", "1");
 </script>
-<script type="text/javascript">
-	function translate(eid, lang) {
-		var o = document.getElementById(eid);
-		var text = o.innerHTML;
-		//alert(text);
-		google.language.detect(text, function(result) {
-	    	if (!result.error) {
-	      		var langCode = result.language;
-				//alert(langCode);
-	  			google.language.translate(text, langCode, lang, function(result) {
-	    			if (result.translation) {
-						//alert(result.translation);
-						o.setAttribute('style', 'background-color: #feffbf;');
-	      				o.innerHTML = '<strong>Translated: </strong>' + result.translation;
-	    			}
-	  			});
-			}
-		});		
-	}
-</script>
 <script type="text/javascript" src="public/js/core.js"></script>
 </head>
 <body>
--- public/js/core.js
+++ public/js/core.js
@@ -12,6 +12,25 @@
 	ui.init(A);
 })(jQuery);*/
 
+function translate(eid, lang) {
+	var o = document.getElementById(eid);
+	var text = o.innerHTML;
+	//alert(text);
+	google.language.detect(text, function(result) {
+    	if (!result.error) {
+      		var langCode = result.language;
+			//alert(langCode);
+  			google.language.translate(text, langCode, lang, function(result) {
+    			if (result.translation) {
+					//alert(result.translation);
+					o.setAttribute('style', 'background-color: #feffbf;');
+      				o.innerHTML = '<strong>Translated: </strong>' + result.translation;
+    			}
+  			});
+		}
+	});		
+}
+
 $(function() {
 	$(".yuvarlak").each(function() {
 		$(this).css({ "border-radius": "7px", "-moz-border-radius": "7px", "-webkit-border-radius": "7px", "-khtml-border-radius": "7px" });
